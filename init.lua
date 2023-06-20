c1b7509f81a6ea302353d3b2b4fd3f09d14e2bb2
SeqSEE <SeqSEE@users.noreply.github.com>
2023-06-20 12:39:50 -0400

    default to wituwu off

--- init.lua
+++ init.lua
@@ -1,52 +1,52 @@
-local what_is_this_uwu = dofile(minetest.get_modpath('what_is_this_uwu')..'/help.lua')
+local what_is_this_uwu = dofile(minetest.get_modpath('what_is_this_uwu') .. '/help.lua')
 
 minetest.register_on_joinplayer(function(player)
 	local meta = player:get_meta()
 
 	local background_id_left = player:hud_add({
 		hud_elem_type = "image",
-		position = {x = 0.5, y = 0},
-		scale = {x = 2, y = 2},
+		position = { x = 0.5, y = 0 },
+		scale = { x = 2, y = 2 },
 		text = '',
-		offset = {x = -50, y = 35},
+		offset = { x = -50, y = 35 },
 	})
 	local background_id_middle = player:hud_add({
 		hud_elem_type = "image",
-		position = {x = 0.5, y = 0},
-		scale = {x = 2, y = 2},
+		position = { x = 0.5, y = 0 },
+		scale = { x = 2, y = 2 },
 		text = '',
-		alignment = {x = 1},
-		offset = {x = -37.5, y = 35},
+		alignment = { x = 1 },
+		offset = { x = -37.5, y = 35 },
 	})
 	local background_id_right = player:hud_add({
 		hud_elem_type = "image",
-		position = {x = 0.5, y = 0},
-		scale = {x = 2, y = 2},
+		position = { x = 0.5, y = 0 },
+		scale = { x = 2, y = 2 },
 		text = '',
-		offset = {x = 0, y = 35},
+		offset = { x = 0, y = 35 },
 	})
 
 	local image_id = player:hud_add({
 		hud_elem_type = "image",
-		position = {x = 0.5, y = 0},
-		scale = {x = 0.3, y = 0.3},
-		offset = {x = -35, y = 35},
+		position = { x = 0.5, y = 0 },
+		scale = { x = 0.3, y = 0.3 },
+		offset = { x = -35, y = 35 },
 	})
 	local name_id = player:hud_add({
 		hud_elem_type = "text",
-		position = {x = 0.5, y = 0},
-		scale = {x = 0.3, y = 0.3},
+		position = { x = 0.5, y = 0 },
+		scale = { x = 0.3, y = 0.3 },
 		number = 0xffffff,
-		alignment = {x = 1},
-		offset = {x = 0, y = 29},
+		alignment = { x = 1 },
+		offset = { x = 0, y = 29 },
 	})
 	local mod_id = player:hud_add({
 		hud_elem_type = "text",
-		position = {x = 0.5, y = 0},
-		scale = {x = 0.3, y = 0.3},
+		position = { x = 0.5, y = 0 },
+		scale = { x = 0.3, y = 0.3 },
 		number = 0xff3c0a,
-		alignment = {x = 1},
-		offset = {x = 0, y = 46},
+		alignment = { x = 1 },
+		offset = { x = 0, y = 46 },
 	})
 
 	meta:set_string('wit:background_left', background_id_left)
@@ -58,7 +58,7 @@ minetest.register_on_joinplayer(function(player)
 	meta:set_string('wit:pointed_thing', 'ignore')
 	meta:set_string('wit:item_type_in_pointer', 'node')
 
-	what_is_this_uwu.register_player(player, player:get_player_name())
+	--what_is_this_uwu.register_player(player, player:get_player_name())
 end)
 
 minetest.register_on_leaveplayer(function(player)
